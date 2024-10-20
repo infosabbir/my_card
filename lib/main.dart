85c280ac348d791056d80d53418421c05d8df4d9
infosabbir <infosabbir9@gmail.com>
2024-10-20 21:17:33 +0600

    Custom fonts added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(
@@ -16,6 +15,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             CircleAvatar(
               radius: 70,
@@ -23,21 +23,64 @@ class MyApp extends StatelessWidget {
             ),
             Text(
               'Sabbir',
-              style: GoogleFonts.pacifico(
+              style: TextStyle(
                 color: Colors.white,
                 fontSize: 40,
                 fontWeight: FontWeight.bold,
+                fontFamily: 'Pacifico',
               ),
             ),
             Text(
               'FLUTTER DEVELOPER',
-              style: GoogleFonts.sourceSans3(
-                color: Colors.teal[100],
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                letterSpacing: 2.5,
+              style: TextStyle(
+                  color: Colors.teal[100],
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  fontFamily: 'Source Sans Pro'),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+              color: Colors.white,
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    '+8801517830512',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20,
+                        fontFamily: 'Source Sans Pro'),
+                  ),
+                ],
               ),
-            )
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+              color: Colors.white,
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'infosabbir9@gmail.com',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20,
+                        fontFamily: 'Source Sans Pro'),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
